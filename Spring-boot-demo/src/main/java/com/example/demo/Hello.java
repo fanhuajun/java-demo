c3b0969f9a300da22271465d21d31df65a7afd72
fanhuajun <357724264@qq.com>
2017-06-06 10:55:42 +0800

    freemarket

--- Spring-boot-demo/src/main/java/com/example/demo/Hello.java
+++ Spring-boot-demo/src/main/java/com/example/demo/Hello.java
@@ -7,7 +7,9 @@
  */
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,11 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
  * description：
  * 
  */
-@RestController
+@Controller
 public class Hello {
     
     @RequestMapping("/hello")
+    @ResponseBody
     public String hello(){
         return "hello world fanhuajun";
+    }
+    
+    @RequestMapping
+    public String freemarket(){
+        return "index";
+        
     }
 }
