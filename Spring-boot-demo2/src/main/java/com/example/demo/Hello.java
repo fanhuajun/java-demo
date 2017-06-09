5055376a805a5c4a910875f0110accdeaf059710
fanhuajun <357724264@qq.com>
2017-06-09 10:06:40 +0800

    s

---
+++ Spring-boot-demo2/src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,36 @@
+/*
+ * @(#)Hello.java 2017年6月6日
+ * 
+ * Copyright (c), 2016 深圳市万睿智能科技有限公司（Shenzhen Wan Rui Intelligent Technology Co., Ltd.）
+ * 
+ * 著作权人保留一切权利，任何使用需经授权。
+ */
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author fanhuajun
+ * @date 2017年6月6日 上午10:26:56
+ * @version V1.0.0
+ * description：
+ * 
+ */
+@Controller
+public class Hello {
+    
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello world fanhuajun";
+    }
+    
+    @RequestMapping
+    public String freemarket(){
+        return "index";
+    }
+}
