63af6466a40bdf2a7a8065aac97fb31a143c393e
357724264@qq.com <fanhuajun@fanhuajun-PDWY.vanke.net.cn>
2017-06-20 19:10:12 +0800

    S

---
+++ spring-boot-starter-web/src/main/java/com/fanhuajun/web/SampleController.java
@@ -0,0 +1,37 @@
+/*
+ * @(#)SampleController.java 2017年6月20日
+ * 
+ * Copyright (c), 2016 深圳市万睿智能科技有限公司（Shenzhen Wan Rui Intelligent Technology Co., Ltd.）
+ * 
+ * 著作权人保留一切权利，任何使用需经授权。
+ */
+package com.fanhuajun.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author fanhuajun
+ * @date 2017年6月20日 下午7:06:58
+ * @version V1.0.0
+ * description：
+ * 
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
