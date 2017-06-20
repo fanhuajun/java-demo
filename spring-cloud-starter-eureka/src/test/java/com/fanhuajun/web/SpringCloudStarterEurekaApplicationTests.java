f640ab575d9365bd77a73a46cf3015978e01f591
357724264@qq.com <fanhuajun@fanhuajun-PDWY.vanke.net.cn>
2017-06-20 23:47:04 +0800

    eureka

---
+++ spring-cloud-starter-eureka/src/test/java/com/fanhuajun/web/SpringCloudStarterEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fanhuajun.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudStarterEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
