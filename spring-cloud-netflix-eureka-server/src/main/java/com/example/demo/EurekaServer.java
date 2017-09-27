23a5c4beb511b6fb7d8c33243ad53f2b744fe71a
fanhuajun <357724264@qq.com>
2017-09-27 14:34:37 +0800

    fanhuajun new

--- spring-cloud-netflix-eureka-server/src/main/java/com/example/demo/EurekaServer.java
+++ spring-cloud-netflix-eureka-server/src/main/java/com/example/demo/EurekaServer.java
@@ -1,13 +1,8 @@
 package com.example.demo;
 
-import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.stereotype.Component;
-
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 
 @EnableEurekaServer
